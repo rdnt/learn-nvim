7d8644fdba96b120dad8385e8b21886bcdbf5237
Tasos Papalyras <tasos@rdnt.dev>
2024-01-25 19:35:10 +0200

    pretty treesitter scope

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,3 +2,14 @@
 require("config.lazy")
 
 vim.opt.relativenumber = false
+vim.opt.shell = "zsh -i"
+vim.opt.ignorecase = true
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.number = true
+
+-- disable treesitter context bottom border and give it a background
+vim.cmd[[hi TreesitterContextBottom gui=NONE]]
+vim.cmd[[hi TreesitterContextBottom guibg=#2a2b3c]]
